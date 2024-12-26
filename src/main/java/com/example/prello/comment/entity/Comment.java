ae97f4a9c9543aa979114ac8058a81ae938098c5
suki <astre15@naver.com>
2024-12-26 19:00:04 +0900

    feat: 댓글 Entity 작성
    1. 댓글 Entity 작성

--- src/main/java/com/example/prello/comment/entity/Comment.java
+++ src/main/java/com/example/prello/comment/entity/Comment.java
@@ -1,7 +1,10 @@
 package com.example.prello.comment.entity;
 
+import com.example.prello.card.Card;
 import com.example.prello.common.BaseEntity;
+import com.example.prello.user.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
@@ -11,4 +14,24 @@ public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id", nullable = false)
+    private Card card;
+
+    public Comment() {}
+
+    @Builder
+    public Comment(String content, User user, Card card) {
+        this.content = content;
+        this.user = user;
+        this.card = card;
+    }
 }

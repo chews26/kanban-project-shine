71f3da2ecc3d857c8dae4f02770005b7b7ea5c4c
shine <112529297+chews26@users.noreply.github.com>
2024-12-26 16:41:34 +0900

    refact: MemberResponseDto @Builder 적용

--- src/main/java/com/example/prello/member/dto/MemberResponseDto.java
+++ src/main/java/com/example/prello/member/dto/MemberResponseDto.java
@@ -1,9 +1,12 @@
 package com.example.prello.member.dto;
 
 import com.example.prello.member.auth.MemberAuth;
+import com.example.prello.member.entity.Member;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Builder
 @Getter
 @RequiredArgsConstructor
 public class MemberResponseDto {
@@ -15,4 +18,13 @@ public class MemberResponseDto {
     private final String email;
 
     private final MemberAuth memberAuth;
+
+    public static MemberResponseDto toDto(Member member) {
+        return MemberResponseDto.builder()
+                .id(member.getId())
+//                .name(member.getUser().getName()) // 예: Member에서 User의 name 가져오기
+//                .email(member.getUser().getEmail()) // 예: Member에서 User의 email 가져오기
+                .memberAuth(member.getAuth())
+                .build();
+    }
 }

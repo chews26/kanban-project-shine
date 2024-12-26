34caf094d8c80602fc8cbdddb2ae6eb57e0879a5
shine <112529297+chews26@users.noreply.github.com>
2024-12-26 17:11:07 +0900

    feat: Board Entity 추가

--- src/main/java/com/example/prello/board/Board.java
+++ src/main/java/com/example/prello/board/Board.java
@@ -1,6 +1,7 @@
 package com.example.prello.board;
 
 import com.example.prello.common.BaseEntity;
+import com.example.prello.workspace.Workspace;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -11,4 +12,14 @@ public class Board extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id")
+    private Workspace workspace;
+
+    private String title;
+
+    private String bg_color;
+
+    private String bg_image;
 }

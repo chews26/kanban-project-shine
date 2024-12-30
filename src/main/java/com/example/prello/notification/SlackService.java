91fc25bcefc0f27e8dbf2adf04c7058fa03c89e5
KM4HS <ac4hsu@gmail.com>
2024-12-31 01:50:22 +0900

    update: 슬랙 서비스 설정 변경

--- src/main/java/com/example/prello/notification/SlackService.java
+++ src/main/java/com/example/prello/notification/SlackService.java
@@ -1,9 +1,12 @@
 package com.example.prello.notification;
 
+import com.example.prello.common.SessionName;
 import com.slack.api.Slack;
 import com.slack.api.model.Attachment;
 import com.slack.api.model.Field;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,20 +20,22 @@ import static com.slack.api.webhook.WebhookPayloads.payload;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class SlackService {
 
+    private final HttpSession session;
     private final Slack slackClient = Slack.getInstance();
 
     @Value("${slack.webhook.url}")
     private String webhookUrl;
 
     // 슬랙 알림 보내는 메서드
-    private void sendSlackAlertErrorLog(Exception e, HttpServletRequest request) {
+    private void sendSlackNotification(String title, HttpServletRequest request, String msg) {
         try {
             slackClient.send(webhookUrl, payload(p -> p
-                    .text("서버 에러 발생! 백엔드 측의 빠른 확인 요망")
+                    .text(title)
                     .attachments(
-                            List.of(generateSlackAttachment(e, request))
+                            List.of(generateSlackAttachment(request, msg))
                     )
             ));
         } catch (IOException slackError) {
@@ -41,16 +46,15 @@ public class SlackService {
     /**
      * 슬랙 메시지 전송
      **/
-    private Attachment generateSlackAttachment(Exception e, HttpServletRequest request) {
+    private Attachment generateSlackAttachment(HttpServletRequest request, String msg) {
         String requestTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").format(LocalDateTime.now());
-        String xffHeader = request.getHeader("X-FORWARDED-FOR");
         return Attachment.builder()
                 .color("ff0000")
-                .title(requestTime + " 발생 에러 로그")
+                .title(requestTime)
                 .fields(List.of(
-                                generateSlackField("Request IP", xffHeader == null ? request.getRemoteAddr() : xffHeader),
+                                generateSlackField("보낸 유저 id", session.getAttribute(SessionName.USER_ID).toString()),
                                 generateSlackField("Request URL", request.getRequestURL() + " " + request.getMethod()),
-                                generateSlackField("Error Message", e.getMessage())
+                                generateSlackField("메세지", msg)
                         )
                 )
                 .build();

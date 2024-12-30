42b7223dbbc130d8cf0b37b56ff5e57b7cd03674
shine <112529297+chews26@users.noreply.github.com>
2024-12-30 22:18:51 +0900

    fix: board 서비스 생성, 수정 , 조회 수정
    보드 조회 시 덱이랑 카드 동시에 조회 가능하도록 수정,
    생성 및 수정 시 잘못된 반환 값으로 반환하고 있던 문제 수정

--- src/main/java/com/example/prello/deck/repository/DeckRepository.java
+++ src/main/java/com/example/prello/deck/repository/DeckRepository.java
@@ -14,4 +14,6 @@ public interface DeckRepository extends JpaRepository<Deck, Long> {
             @Param("boardId") Long boardId,
             @Param("startOrder") int startOrder,
             @Param("endOrder") int endOrder);
+
+    List<Deck> findByBoardId(Long boardId);
 }
